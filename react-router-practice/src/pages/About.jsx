a7936695fc25608953ea97d1fb6803beec855d1b
bxxloob_-@naver.com <nanaukim@naver.com>
2022-06-21 16:37:28 +0900

    delete unnecessary files

--- react-router-practice/src/pages/About.jsx
+++
@@ -1,3 +0,0 @@
-export default function About() {
-  return <div>About 페이지입니다.</div>;
-}
--- react-router-practice/src/pages/Profile.jsx
+++
@@ -1,4 +0,0 @@
-export default function Profile(props) {
-  console.log(props)
-  return <div>Profile 페이지입니다.</div>;
-}
